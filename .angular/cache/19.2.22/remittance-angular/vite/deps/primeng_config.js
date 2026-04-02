4922964c8e48f84b1887090fa034730bf5eb43fb
Aashish Pudasaini <apudasaini@smartpayservices.eu>
2026-04-02 07:19:02 +0300

    .

---
+++ .angular/cache/19.2.22/remittance-angular/vite/deps/primeng_config.js
@@ -0,0 +1,20 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-URBTZYXA.js";
+import "./chunk-2QMUFQQW.js";
+import "./chunk-HYLSEEKY.js";
+import "./chunk-ZCADONXW.js";
+import "./chunk-WKSQZMM4.js";
+import "./chunk-QO67QQ3F.js";
+import "./chunk-WMWTRTCA.js";
+import "./chunk-PADBOZH3.js";
+import "./chunk-S5KMCARS.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
